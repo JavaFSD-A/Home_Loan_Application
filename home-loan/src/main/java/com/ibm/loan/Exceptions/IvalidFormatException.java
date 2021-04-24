b84473a2dfe5cbf60013f6b3168aaba9e27b7c1d
*Satyasri-Samayamanthula* <ibmjavafsdmr16@Javafsdnh260.iiht.tech>
2021-04-24 18:07:22 +0530

    capital Class

---
+++ home-loan/src/main/java/com/ibm/loan/Exceptions/IvalidFormatException.java
@@ -0,0 +1,19 @@
+/**
+ * @author satya
+ */
+package com.ibm.loan.Exceptions;
+
+public class IvalidFormatException extends Exception {
+
+	public IvalidFormatException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public IvalidFormatException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
