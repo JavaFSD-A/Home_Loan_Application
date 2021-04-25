ef795824d44b922e0a161ec7a028d34de2918fb1
Rajarshi Bose <brajarshibose1997@gmail.com>
2021-04-25 16:35:48 +0530

    Admin and Admin Repository Class Filled

--- home-loan/src/main/java/com/yourcastle/homeloan/repo/AdminRepository.java
+++ home-loan/src/main/java/com/yourcastle/homeloan/repo/AdminRepository.java
@@ -1,5 +1,13 @@
 package com.yourcastle.homeloan.repo;
 
-public class AdminRepository {
+import java.util.List;
+
+import com.yourcastle.homeloan.entity.Loan;
+
+public interface AdminRepository {
+	
+	public List<Loan> getLoanDeatils();
+	public List<Loan> getLoan(boolean status);
+	public boolean UpdateStatus(int age_limit,int min_sal,int max_tenure);
 
 }
