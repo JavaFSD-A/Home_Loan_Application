240ace993a0de1a493e44c6325fea79f6c4bafd0
Rajarshi Bose <brajarshibose1997@gmail.com>
2021-04-25 22:19:21 +0530

    Admin Service Updated

--- home-loan/src/main/java/com/yourcastle/homeloan/service/AdminService.java
+++ home-loan/src/main/java/com/yourcastle/homeloan/service/AdminService.java
@@ -1,5 +1,19 @@
 package com.yourcastle.homeloan.service;
 
+import java.util.List;
+
+import com.yourcastle.homeloan.entity.Customer;
+
 public interface AdminService {
+	
+	public boolean approveLoan(int loanId);
+
+	public boolean rejectLoan(int loanId);
+	
+	public Customer getCustomerbyId(int cust_id);
+	
+	public List<Customer> getAllCustomers();
+	
+	public boolean updatecustomerLoanStatus(int cust_id);
 
 }
--- home-loan/src/main/java/com/yourcastle/homeloan/service/AdminServiceImpl.java
+++ home-loan/src/main/java/com/yourcastle/homeloan/service/AdminServiceImpl.java
@@ -1,5 +1,40 @@
 package com.yourcastle.homeloan.service;
 
-public class AdminServiceImpl {
+import java.util.List;
+
+import com.yourcastle.homeloan.entity.Customer;
+
+public class AdminServiceImpl implements AdminService{
+
+	@Override
+	public boolean approveLoan(int loanId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean rejectLoan(int loanId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Customer getCustomerbyId(int cust_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Customer> getAllCustomers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean updatecustomerLoanStatus(int cust_id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 
 }
