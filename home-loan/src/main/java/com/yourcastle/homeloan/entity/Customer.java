bf4b83572ba10bf9aa8ec2596c1f438b6a0299d3
Tarishi Geetey <tgeeteyt@gmail.com>
2021-04-28 09:27:20 +0530

    ok

--- home-loan/src/main/java/com/yourcastle/homeloan/entity/Customer.java
+++ home-loan/src/main/java/com/yourcastle/homeloan/entity/Customer.java
@@ -3,9 +3,8 @@
  */
 package com.yourcastle.homeloan.entity;
 
-import java.util.ArrayList;
+
 import java.util.Date;
-import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -14,13 +13,9 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
