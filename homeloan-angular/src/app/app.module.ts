e0823d879e416f1571f18c293ef3f71ba378c526
Tarishi Geetey <tgeeteyt@gmail.com>
2021-05-05 15:42:45 +0530

    adding pludins and app.modules to be used

--- homeloan-angular/src/app/app.module.ts
+++ homeloan-angular/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { UserComponent } from './user/user.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
